069136a0c0c37156c60780bea283fcd4616c1b98
George Papagapitos <george.g.papagapitos@gmail.com>
2021-07-18 12:34:45 -0500

    added state for navbar style

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,18 @@
 import './NavBar.scss';
 import { Search, Notifications, ArrowDropDown } from '@material-ui/icons';
+import { useState } from 'react';
 
 const NavBar = () => {
+
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => window.onscroll = null;
+  };
+
   return (
-    <div className="navbar">
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
         <div className="left">
           <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="netflix-logo" />
@@ -17,7 +26,13 @@ const NavBar = () => {
           <span>KID</span>
           <Notifications className="icon" />
           <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb=2&w=500" alt="profile-pic" />
-          <ArrowDropDown className="icon" />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
