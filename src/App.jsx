0517f24b31d0a1292d46560954979198786c5346
George Papagapitos <george.g.papagapitos@gmail.com>
2021-07-19 17:40:54 -0500

    register page styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.scss';
 import Home from './pages/Home/Home';
+import Register from './pages/Register/Register';
 import Watch from './pages/Watch/Watch';
 
 export default function App() {
-  return (<Watch />);
+  return (<Register />);
 }
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,24 @@
-import './Register.scss';
+import "./Register.scss";
 
 
 const Register = () => {
   return (
     <div className="register">
-
+      <div className="top">
+        <div className="wrapper">
+          <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="logo" />
+          <button className="login-button">Sign In</button>
+        </div>
+        <div className="container">
+          <h1>Unlimited movies, TV shows, and more.</h1>
+          <h2>Watch anywhere. Cancel anytime.</h2>
+          <p>Ready to watch?  Enter your email to create or restart your membership.</p>
+          <div className="input">
+            <input type="email" placeholder="email address" />
+            <button className="register-button">Get Started</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
