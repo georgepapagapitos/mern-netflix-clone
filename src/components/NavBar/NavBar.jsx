e2e10dd5991ae2fc624c1cdf35151f2f2f15310b
George Papagapitos <george.g.papagapitos@gmail.com>
2021-07-19 18:20:36 -0500

    fixed typo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -23,7 +23,7 @@ export default function NavBar() {
         </div>
         <div className="right">
           <Search className="icon" />
-          <span>KID</span>
+          <span>DVD</span>
           <Notifications className="icon" />
           <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb=2&w=500" alt="profile-pic" />
           <div className="profile">
